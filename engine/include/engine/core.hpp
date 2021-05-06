76de25f638777e41541df2bba4bbc4238030e089
TheBeautifulOrc <luai.malek@gmail.com>
2021-05-06 19:01:40 +0200

    Finished engine build script rework Now works for any number of different modules

--- engine/include/engine/core.hpp
+++ engine/include/engine/core.hpp
@@ -1,2 +1,10 @@
 #pragma once
 
+#include "core/engine_component.hpp"
+#include "core/application_layer/application.hpp"
+#include "core/basics/hierarchy.hpp"
+#include "core/event_system/event.hpp"
+#include "core/event_system/event_handler.hpp"
+#include "core/logging_system/logger.hpp"
+#include "core/logging_system/spdlogger.hpp"
+#include "core/application_layer/window/window_manager.hpp"
